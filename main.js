a7dfaf8b77be6cd47e8f70fe5f1999c962b536c8
HyperGamesDev <hypergamesdev@gmail.com>
2021-04-10 16:38:00 +0200

    About Me page

---
+++ main.js
@@ -0,0 +1,6 @@
+var title="About Me";
+var desc="My name's Maciej, but I prefer to be called Hyper<br>Im a 17 year old developer(as of 2021)<br>that found a passion of creating games<br>back in 2017 when I was a 14 year old<br><br>I created my first game in GMS following a tutorial<br>trying to change some stuff on the way as I learned.<br>I published my first game on 31st of August 2017<br>called Best Space Shooter 2017(I know, I know)<br>then up till December 2019 I pushed through and updated<br>the game adding a lot of new features(and the name changed to SSS77<br><br>From January 2020 I started working in Unity and I'm still<br>working on my biggest project called SSS222";
+
+var buttonText1="About Me";
+var buttonText2="Portfolio";
+var buttonText3="Contact";
