f74def12b7aa5ae6757a95bf49a6235a14cdf9be
HyperGamesDev <hypergamesdev@gmail.com>
2021-04-12 17:48:17 +0200

    Contacts on AboutMe page + All portfolio projects
    Cleaned up a lot of the mess

--- main.js
+++ main.js
@@ -1,27 +1,46 @@
 var title="About Me";
-var desc="My name's Maciej, but I prefer to be called Hyper<br>Im a 17 year old developer(as of 2021)<br>that found a passion of creating games<br>back in 2017 when I was a 14 year old<br><br>I created my first game in GMS following a tutorial<br>trying to change some stuff on the way as I learned.<br>I published my first game on 31st of August 2017<br>called Best Space Shooter 2017(I know, I know)<br>then up till December 2019 I pushed through and updated<br>the game adding a lot of new features(and the name changed to SSS77)<br><br>From January 2020 I started working in Unity and I'm still<br>working on my biggest project called SSS222";
-
-var pageName1="About Me";
-var pageName2="Portfolio";
-var pageName3="Contact";
+var desc="My name's Maciej, but I prefer to be called Hyper<br>\
+Im a 17 year old developer(as of 2021)<br>\
+that found a passion of creating games<br>\
+back in 2017 when I was a 14 year old<br><br>\
+I created my first game in GMS following a tutorial<br>\
+trying to change some stuff on the way as I learned.<br>\
+I published my first game on 31st of August 2017<br>\
+called Best Space Shooter 2017(I know, I know..)<br>\
+then up till December 2019 I pushed through and updated<br>\
+the game adding a lot of new features(and the name changed to SSS77)<br><br>\
+From January 2020 I started working in Unity and I'm still<br>\
+working on my biggest project called SSS222";
 
 function buttonFunc1(){location.href='index.html';}
 function buttonFunc2(){location.href='portfolio.html';}
-function buttonFunc3(){location.href='contact.html';}
 
 function checkPage(){
 	var path = window.location.pathname;
 	var page = path.split("/").pop();
 	console.log(page);
 	if(page=='portfolio.html'){
-		title=pageName2;
+		title="Portfolio";
 		desc="";
-	}else if(page=='contact.html'){
-		title=pageName3;
-		desc="";
+		document.getElementById("sss222date").innerHTML="03.2021-??.????";
+		document.getElementById("glitchedoutDate").innerHTML="03.2018-11.2018";
+		document.getElementById("sss77date").innerHTML="08.2017-12.2019";
+	}else{
+		document.getElementById("twitter").innerHTML="<img src='img/twitter.png'>";
+		document.getElementById("github").innerHTML="<img src='img/github.png'>";
+		document.getElementById("linkedin").innerHTML="<img src='img/linkedin.png'>";
 	}
+	document.getElementById("title").innerHTML=title;
+	document.getElementById("desc").innerHTML=desc;
 }
 function sss222Button(){window.open("https://hypergamesdev.github.io/sss222");}
+function sss77Button(){window.open("https://github.com/HyperGamesDev/sss77");}
+function glitchedoutButton(){window.open("https://github.com/HyperGamesDev/GlitchedOut-GMS-0.1-");}
+
+function twitterButton(){window.open("https://twitter.com/HyperLemonPL");}
+function githubButton(){window.open("https://github.com/HyperGamesDev");}
+function linkedinButton(){window.open("https://linkedin.com/in/hypergamesdev");}
+
 
 function docWrite(string){document.write(string);}
 function sleep(milliseconds) {
