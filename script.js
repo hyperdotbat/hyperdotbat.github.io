90bad8c9c6ebac55c9a4ba8684693d8e06989fab
HyperGamesDev <HyperGamesDev@gmail.com>
2021-10-29 15:59:11 +0200

    Overhaul 29.10.2021

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var title="About Me";
 var desc="My name's Maciej, but I prefer to be called <b><i>Hyper</i></b><br>\
-Im an 18 year old developer(as of 2021)<br>\
+Im an 18 year old developer<br>\
 that found the passion of creating games<br>\
 at the age of 14 back in 2017<br><br>\
 I created my first game in GMS following a tutorial<br>\
@@ -19,43 +19,59 @@ function checkPage(){
 	var path = window.location.pathname;
 	var page = path.split("/").pop();
 	console.log(page);
-	document.getElementById("index").href="index.html";
-	document.getElementById("portfolio").href="portfolio.html";
+	if(document.getElementById("index")!=null)document.getElementById("index").href="index.html";
+	if(document.getElementById("portfolio")!=null)document.getElementById("portfolio").href="portfolio.html";
 	if(page=='portfolio.html'){
 		title=pageName2;
 		desc="";
-		document.getElementById("sss222Button").href="https://hypergamesdev.github.io/sss222";
-		document.getElementById("sss222date").innerHTML="03.2020 - ??.????";
-		document.getElementById("glitchoutButton").href="https://hypergamesdev.github.io/glitchout";
-		document.getElementById("glitchoutDate").innerHTML="06.2021 - ??.????";
-		document.getElementById("glitchedoutButton").href="https://github.com/HyperGamesDev/GlitchedOut-GMS-0.1-";
-		document.getElementById("glitchedoutDate").innerHTML="03.2018 - 11.2018";
-		document.getElementById("sss77Button").href="https://github.com/HyperGamesDev/sss77";
-		document.getElementById("sss77Date").innerHTML="08.2017 - 12.2019";
-		document.getElementById("planetoidButton").href="https://github.com/HyperGamesDev/mc-planetoids";
-		document.getElementById("planetoidDate").innerHTML="06.2020 - ??.????";
+		if(document.getElementById("sss222Button")!=null)
+			document.getElementById("sss222Button").href="https://hypergamesdev.github.io/sss222";
+		if(document.getElementById("sss222date")!=null)
+			document.getElementById("sss222date").innerHTML="03.2020 - ??.????";
+		if(document.getElementById("glitchoutButton")!=null)
+			document.getElementById("glitchoutButton").href="https://hypergamesdev.github.io/glitchout";
+		if(document.getElementById("glitchoutDate")!=null)
+			document.getElementById("glitchoutDate").innerHTML="06.2021 - ??.????";
+		if(document.getElementById("glitchedoutButton")!=null)
+			document.getElementById("glitchedoutButton").href="https://github.com/HyperGamesDev/GlitchedOut-GMS-0.1-";
+		if(document.getElementById("glitchedoutDate")!=null)
+			document.getElementById("glitchedoutDate").innerHTML="03.2018 - 11.2018";
+		if(document.getElementById("sss77Button")!=null)
+			document.getElementById("sss77Button").href="https://github.com/HyperGamesDev/sss77";
+		if(document.getElementById("sss77Date")!=null)
+			document.getElementById("sss77Date").innerHTML="08.2017 - 12.2019";
+		if(document.getElementById("planetoidButton")!=null)
+			document.getElementById("planetoidButton").href="https://github.com/HyperGamesDev/mc-planetoids";
+		if(document.getElementById("planetoidDate")!=null)
+			document.getElementById("planetoidDate").innerHTML="06.2020 - ??.????";
 	}else{
-		document.getElementById("allmylinks").innerHTML="<img src='img/allmylinks.png'>";
-		document.getElementById("allmylinks").href="https://my.bio/hypergamesdev";
-		document.getElementById("twitter").innerHTML="<img src='img/twitter.png'>";
-		document.getElementById("twitter").href="https://twitter.com/HyperLemonPL";
-		document.getElementById("github").innerHTML="<img src='img/github.png'>";
-		document.getElementById("github").href="https://github.com/hypergamesdev";
-		//document.getElementById("patreon").innerHTML="<img src='img/patreon.png'>";
-		//document.getElementById("patreon").href="https://patreon.com/HyperGamesDev";
-		document.getElementById("kofi").innerHTML="<img src='img/kofi.png'>";
-		document.getElementById("kofi").href="https://buymeacoffee.com/HyperGamesDev";
-		document.getElementById("linkedin").innerHTML="<img src='img/linkedin.png'>";
-		document.getElementById("linkedin").href="https://linkedin.com/in/hypergamesdev";
-		document.getElementById("cv_EN").innerHTML="CV_EN";
-		document.getElementById("cv_EN").href="redirects/cv_EN.html";
-		document.getElementById("cv_PL").innerHTML="CV_PL";
-		document.getElementById("cv_PL").href="redirects/cv_PL.html";
+		if(document.getElementById("allmylinks")!=null)
+			document.getElementById("allmylinks").innerHTML="<img src='img/allmylinks.png'>";
+			document.getElementById("allmylinks").href="https://my.bio/hypergamesdev";
+		}if(document.getElementById("twitter")!=null){
+			document.getElementById("twitter").innerHTML="<img src='img/twitter.png'>";
+			document.getElementById("twitter").href="https://twitter.com/HyperLemonPL";
+		}if(document.getElementById("github")!=null){
+			document.getElementById("github").innerHTML="<img src='img/github.png'>";
+			document.getElementById("github").href="https://github.com/hypergamesdev";
+		}if(document.getElementById("patreon")!=null){
+			document.getElementById("patreon").innerHTML="<img src='img/patreon.png'>";
+			document.getElementById("patreon").href="https://patreon.com/HyperGamesDev";
+		}if(document.getElementById("kofi")!=null){
+			document.getElementById("kofi").innerHTML="<img src='img/kofi.png'>";
+			document.getElementById("kofi").href="https://buymeacoffee.com/HyperGamesDev";
+		}if(document.getElementById("linkedin")!=null){
+			document.getElementById("linkedin").innerHTML="<img src='img/linkedin.png'>";
+			document.getElementById("linkedin").href="https://linkedin.com/in/hypergamesdev";
+		}if(document.getElementById("cv_EN")!=null){
+			document.getElementById("cv_EN").innerHTML="CV_EN";
+			document.getElementById("cv_EN").href="redirects/cv_EN.html";
+		}if(document.getElementById("cv_PL")!=null){
+			document.getElementById("cv_PL").innerHTML="CV_PL";
+			document.getElementById("cv_PL").href="redirects/cv_PL.html";
 	}
-	document.getElementById("title").innerHTML=title;
-	document.getElementById("desc").innerHTML=desc;
-	
-	
+	if(document.getElementById("title")!=null)document.getElementById("title").innerHTML=title;
+	if(document.getElementById("desc")!=null)document.getElementById("desc").innerHTML=desc;
 }
 
 function redirect_cvEN(){window.open("https://raw.githubusercontent.com/HyperGamesDev/HyperGamesDev/master/Grafika%20Social/CV-HyperGamesDev_EN.png","_self");}
