41f455be03cd2caf6eedab50d1f78aff3e965e99
Hyper! <hypergamesdev@gmail.com>
2022-04-18 13:23:59 +0200

    18.04.2022 - jQuery, cleanup etc

--- script.js
+++ script.js
@@ -4,88 +4,74 @@ An 18 year old developer<br>\
 that found the passion of creating games<br>\
 at the age of 14 back in 2017<br><br>\
 \
-I created my first game in GMS following a tutorial<br>\
+I created my first game in GMS following a HeartBest tutorial<br>\
 trying to change some stuff on the way as I learned.<br>\
-I published my first game on 31st of August 2017<br>\
+I published it on 31st of August 2017<br>\
 called <i>Best Space Shooter 2017</i> (lmao I know)<br>\
 then up till December 2019 I pushed through and updated<br>\
-it adding a lot of new features (and the name changed to <i>SSS77</i>)<br><br>\
+it adding a lot of new features and changed the name to <a href='https://github.com/HyperGamesDev/sss77'><i>SSS77</i></a><br><br>\
 \
 From January 2020 I started working in Unity and still am<br>\
 on my biggest project called <a href='https://hypergamesdev.github.io/sss222' target='_blank'><b>SSS222</b></a><br><br>\
-Im also the founder of <a href='https://hyperlemonstudios.github.io'>HyperLemonStudios</a>\
+Im also the founder of <a href='https://hyperlemonstudios.github.io'><i>HyperLemonStudios</i></a><br><br>\
+\
+If you want to know more about me the best I can say<br>\
+besides gamedev and gaming in general is<br>\
+Im interested in Japanese culture and the language!\
 ";
+var footer="HyperGamesDev® | HyperLemonStudios® | Maciej Krefft | 2017-2022";
 
 var pageName1="About Me";
 var pageName2="Portfolio";
 
-function checkPage(){
-	var path = window.location.pathname;
-	var page = path.split("/").pop();
-	console.log(page);
-	if(document.getElementById("index")!=null)document.getElementById("index").href="index.html";
-	if(document.getElementById("portfolio")!=null)document.getElementById("portfolio").href="portfolio.html";
+setTimeout(function Setup(){
+	let path=window.location.pathname;
+	let page=path.split("/").pop();
+	$("#index").children("span").html(pageName1);
+	$("#portfolio").children("span").html(pageName2);
+	$("#index").attr("href","index.html");
+	$("#portfolio").attr("href","portfolio.html");
+	$(".footer").children("footer").html(footer);
+	
 	if(page=='portfolio.html'){
 		title=pageName2;
 		desc="";
-		if(document.getElementById("sss222Button")!=null)
-			document.getElementById("sss222Button").href="https://hypergamesdev.github.io/sss222";
-		if(document.getElementById("sss222date")!=null)
-			document.getElementById("sss222date").innerHTML="03.2020 - ??.????";
-		if(document.getElementById("glitchoutButton")!=null)
-			document.getElementById("glitchoutButton").href="https://hypergamesdev.github.io/glitchout";
-		if(document.getElementById("glitchoutDate")!=null)
-			document.getElementById("glitchoutDate").innerHTML="06.2021 - ??.????";
-		if(document.getElementById("glitchedoutButton")!=null)
-			document.getElementById("glitchedoutButton").href="https://github.com/HyperGamesDev/GlitchedOut-GMS-0.1-";
-		if(document.getElementById("glitchedoutDate")!=null)
-			document.getElementById("glitchedoutDate").innerHTML="03.2018 - 11.2018";
-		if(document.getElementById("sss77Button")!=null)
-			document.getElementById("sss77Button").href="https://github.com/HyperGamesDev/sss77";
-		if(document.getElementById("sss77Date")!=null)
-			document.getElementById("sss77Date").innerHTML="07.2017 - 12.2019";
-		if(document.getElementById("planetoidButton")!=null)
-			document.getElementById("planetoidButton").href="https://github.com/HyperGamesDev/mc-planetoids";
-		if(document.getElementById("planetoidDate")!=null)
-			document.getElementById("planetoidDate").innerHTML="06.2020 - ??.????";
+		$("#sss222Button").attr("href","https://hypergamesdev.github.io/sss222");
+		$("#sss222date").html("03.2020 - ??.????");
+		$("#glitchoutButton").attr("href","https://hypergamesdev.github.io/glitchout");
+		$("#glitchoutDate").html("06.2021 - ??.????");
+		
+		$("#glitchedoutButton").attr("href","https://github.com/HyperGamesDev/GlitchedOut-GMS-0.1-");
+		$("#glitchedoutDate").html("03.2018 - 11.2019");
+		$("#sss77Button").attr("href","https://github.com/HyperGamesDev/sss77");
+		$("#sss77Date").html("07.2017 - 12.2019");
+
+		$("#planetoidButton").attr("href","https://github.com/HyperGamesDev/mc-planetoids");
+		$("#planetoidDate").html("06.2020 - ??.????");
 	}else{
-		if(document.getElementById("allmylinks")!=null)
-			document.getElementById("allmylinks").innerHTML="<img src='img/carrd.png'>";
-			document.getElementById("allmylinks").href="https://hyperr.carrd.co";
-		}if(document.getElementById("twitter")!=null){
-			document.getElementById("twitter").innerHTML="<img src='img/twitter.png'>";
-			document.getElementById("twitter").href="https://twitter.com/HyperrGamesDev";
-		}if(document.getElementById("github")!=null){
-			document.getElementById("github").innerHTML="<img src='img/github.png'>";
-			document.getElementById("github").href="https://github.com/hypergamesdev";
-		}if(document.getElementById("patreon")!=null){
-			document.getElementById("patreon").innerHTML="<img src='img/patreon.png'>";
-			document.getElementById("patreon").href="https://patreon.com/HyperGamesDev";
-		}if(document.getElementById("kofi")!=null){
-			document.getElementById("kofi").innerHTML="<img src='img/kofi.png'>";
-			document.getElementById("kofi").href="https://buymeacoffee.com/HyperGamesDev";
-		}if(document.getElementById("linkedin")!=null){
-			document.getElementById("linkedin").innerHTML="<img src='img/linkedin.png'>";
-			document.getElementById("linkedin").href="https://linkedin.com/in/hypergamesdev";
-		}if(document.getElementById("cv_EN")!=null){
-			document.getElementById("cv_EN").innerHTML="CV_EN";
-			document.getElementById("cv_EN").href="redirects/cv_EN.html";
-		}if(document.getElementById("cv_PL")!=null){
-			document.getElementById("cv_PL").innerHTML="CV_PL";
-			document.getElementById("cv_PL").href="redirects/cv_PL.html";
+		$("#allmylinks").html("<img src='img/carrd.png'>");
+		$("#allmylinks").attr("href","https://hyperr.carrd.co");
+		$("#twitter").html("<img src='img/twitter.png'>");
+		$("#twitter").attr("href","https://twitter.com/HyperrGamesDev");
+		$("#github").html("<img src='img/github.png'>");
+		$("#github").attr("href","https://github.com/HyperGamesDev");
+		$("#patreon").html("<img src='img/patreon.png'>");
+		$("#patreon").attr("href","https://patreon.com/HyperGamesDev");
+		$("#kofi").html("<img src='img/kofi.png'>");
+		$("#kofi").attr("href","https://buymeacoffee.com/HyperGamesDev");
+		$("#linkedin").html("<img src='img/linkedin.png'>");
+		$("#linkedin").attr("href","https://linkedin.com/in/hypergamesdev");
+
+		$("#cv_EN").html("CV_EN");
+		$("#cv_EN").attr("href","redirects/cv_EN.html");
+		$("#cv_PL").html("CV_PL");
+		$("#cv_PL").attr("href","redirects/cv_PL.html");
 	}
-	if(document.getElementById("title")!=null)document.getElementById("title").innerHTML=title;
-	if(document.getElementById("desc")!=null)document.getElementById("desc").innerHTML=desc;
-}
+	$("#title").html(title);
+	$("#desc").html(desc);
+},100);
 
 function redirect_cvEN(){window.open("https://raw.githubusercontent.com/HyperGamesDev/HyperGamesDev/master/Grafika%20Social/CV-HyperGamesDev_EN.png","_self");}
 function redirect_cvPL(){window.open("https://raw.githubusercontent.com/HyperGamesDev/HyperGamesDev/master/Grafika%20Social/CV-HyperGamesDev_PL.png","_self");}
 
-function docWrite(string){document.write(string);}
-function sleep(milliseconds) {
-  const date = Date.now();
-  let currentDate = null;
-  do {
-    currentDate = Date.now();
-  } while (currentDate - date < milliseconds);
-}
+const delay = async (ms = 1000) => new Promise(resolve => setTimeout(resolve, ms));
